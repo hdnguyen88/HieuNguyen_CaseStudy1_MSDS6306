f30443cf143183b3863a7b3d3738b31c2b639de4
Hieu Nguyen <Hieu Nguyen>
2017-03-23 00:42:11 -0500

    Update comments

--- DownloadData.R
+++ DownloadData.R
@@ -7,9 +7,11 @@ download.file(file2, destfile = "EDU.csv")
 
 
 ##Format the columns
+##Skip column 3 because it is an empty column
 
 gdpdata <- read.csv("GDP.csv", skip = 4, nrows = 190)
 edudata <- read.csv("EDU.csv")
 gdpdata <- gdpdata[, c(1,2,4,5)]
 colnames(gdpdata) <- c("CountryCode", "Rank", "Country.Name", "GDP.Value")
 gdpdata$GDP.Value <- as.numeric(gsub(",", "", gdpdata$GDP.Value))
+
