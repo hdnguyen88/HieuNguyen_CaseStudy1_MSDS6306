7c3eced690aacdbe16a7733445abacdbd7791f4f
Hieu Nguyen <Hieu Nguyen>
2017-03-23 00:13:23 -0500

    Download Data

---
+++ DownloadData.R
@@ -0,0 +1,4 @@
+file1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(file1, destfile = "GDP.csv")
+file2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(file2, destfile = "EDU.csv")
