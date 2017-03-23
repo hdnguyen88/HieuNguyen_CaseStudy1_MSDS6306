d9581d54e7d423c6adc3ec11ff418859b296a983
hdnguyen88 <hdnguyen@mail.smu.edu>
2017-03-23 00:37:26 -0500

    Update DownloadData.R

--- DownloadData.R
+++ DownloadData.R
@@ -1,4 +1,15 @@
+##Download the files
+
 file1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 download.file(file1, destfile = "GDP.csv")
 file2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
 download.file(file2, destfile = "EDU.csv")
+
+
+##Format the columns
+
+gdpdata <- read.csv("GDP.csv", skip = 4, nrows = 190)
+edudata <- read.csv("EDU.csv")
+gdpdata <- gdpdata[, c(1,2,4,5)]
+colnames(gdpdata) <- c("CountryCode", "Rank", "Country.Name", "GDP.Value")
+gdpdata$GDP.Value <- as.numeric(gsub(",", "", gdpdata$GDP.Value))
